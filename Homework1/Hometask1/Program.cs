23c887c738f463cfff2b2f4ad853dbb4bc99a6a6
Nazar <stellon256@gamil.com>
2023-10-16 19:38:19 +0300

    Сделана первая задача

---
+++ Homework1/Hometask1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите метод CompareNumbers, который на вход принимает два числа и выводит, какое число большее, а какое меньшее.
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+ 
+if(number1 > number2)
+{
+    Console.WriteLine($"Первое число {number1} больше второго {number2}");
+}
+else if (number1 < number2)
+{
+    Console.WriteLine($"Первое число {number1} меньше второго {number2}");
+}
+else
+{
+    Console.WriteLine($"Оба числа равны {number1}");
+}
