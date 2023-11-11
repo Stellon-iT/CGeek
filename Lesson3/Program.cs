8b56df4c6e7d7c9b21ef9adbd972b543fbcead01
Nazar <stellon256@gamil.com>
2023-11-11 17:47:30 +0300

    Добавлены данные

---
+++ Lesson3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+while (x > 0)
+{
+    x /= 10;
+    count++;
+}
+Console.WriteLine($"Количество цифр = {count}");
