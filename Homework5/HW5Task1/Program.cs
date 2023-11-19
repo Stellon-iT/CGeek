df5abcb29c095be900c625c4558c0d22a32ea4b1
Nazar <stellon256@gamil.com>
2023-11-19 19:50:08 +0300

    Added new Homework

---
+++ Homework5/HW5Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿string ReadDataStr(string msg)
+{
+    Console.WriteLine(msg);
+    return Console.ReadLine() ?? "0";
+}
+
+int CountPositivNumber(string str)
+{
+    int count = 0;
+    string strWoSp = str.Replace(" ", "");
+    string[] strArr = strWoSp.Split(",");
+    int[] intArr = new int[strArr.Length];
+    for (int i = 0; i < intArr.Length; i++) intArr[i] = int.Parse(strArr[i]);
+    for (int i = 0; i < intArr.Length; i++) if (intArr[i] > 0) count++;
+    return count;
+}
+
+string sequenceNumbers = ReadDataStr("Введите последовательность чисел: ");
+int countPosNum = CountPositivNumber(sequenceNumbers);
+
+Console.WriteLine("Кол-во положительных чисел равно: " + countPosNum);
---
+++ Homework5/HW5Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿double ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return Convert.ToDouble(Console.ReadLine() ?? "0");
+}
+
+double xVolume(double b1, double k1, double b2, double k2)
+{
+    double xVol = (b2 - b1) / (k1 - k2);
+    return xVol;
+}
+
+double yVolume(double b1, double k1, double x)
+{
+    double y = k1 * x + b1;
+    return y;
+}
+
+double b1 = ReadData("Введите b1: ");
+double k1 = ReadData("Введите k1: ");
+double b2 = ReadData("Введите b2: ");
+double k2 = ReadData("Введите k2: ");
+
+double x = xVolume(b1, k1, b2, k2);
+double y = yVolume(b1, k1, x);
+
+Console.WriteLine("Значение Х = " + x);
+Console.WriteLine("Значение Y = " + y);
